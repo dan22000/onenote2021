66ed00bba7d444f45e3abf36bdd9d8d37c9abe3e
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2021-12-11 10:18:18 +0100

    Updated project

--- app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
+++ app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
@@ -1,8 +1,10 @@
 package com.wohlmuth.onenote2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 
 class ListActivity : AppCompatActivity(), View.OnClickListener {
@@ -17,6 +19,15 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_list, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add) {
+            val intent = Intent(this, NoteEditActivity::class.java)
+            startActivity(intent)
+        }
 
         return true
     }
