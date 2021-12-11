5927c3e2a40c4efa8ee25124016f0dc5b540ab97
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2021-12-11 10:31:33 +0100

    Updated project

--- app/src/main/java/com/wohlmuth/onenote2021/Preferences.kt
+++ app/src/main/java/com/wohlmuth/onenote2021/Preferences.kt
@@ -6,7 +6,18 @@ class Preferences {
 
     companion object {
         const val PREFERENCES_USER_LOGGED_IN = "user_logged_in"
+        const val PREFERENCES_NOTE_TITLE = "note_title"
         const val PREFERENCES_NAME = "preferences_note"
+    }
+
+    fun setNoteTitle(context: Context, noteTitle: String) {
+        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+        preferences.edit().putString(PREFERENCES_NOTE_TITLE, noteTitle).apply()
+    }
+
+    fun getNoteTitle(context: Context): String? {
+        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+        return preferences.getString(PREFERENCES_NOTE_TITLE, null)
     }
 
     fun setUserLoggedIn(context: Context, loggedIn: Boolean) {
