e79b1e418f2f2d8f9ed1b15a5cde2437dab484d4
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2021-12-11 14:02:55 +0100

    Updated project

--- app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
+++ app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
@@ -1,17 +1,56 @@
 package com.wohlmuth.onenote2021
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.reflect.Array
+
 
 class ListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        // Find view by id
+        val listView = findViewById<ListView>(R.id.lvNotes)
+
+        // Create list of strings
+        val notes = listOf(
+            "Pizza",
+            "Döner",
+            "Cars",
+            "Guns",
+            "Girls",
+            "Brokkoli",
+            "Pizza",
+            "Döner",
+            "Cars",
+            "Guns",
+            "Girls",
+            "Brokkoli",
+            "Pizza",
+            "Döner",
+            "Cars",
+            "Guns",
+            "Girls",
+            "Brokkoli",
+            "Eis"
+        )
+
+        // Init adapter
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, notes
+        )
+
+        // Set adapter on listView
+        listView.adapter = adapter
     }
 
     override fun onResume() {
