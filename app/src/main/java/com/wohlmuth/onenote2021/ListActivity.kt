e92ffb60bf060db87cb8c5b425cb042b3d39a151
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2021-12-17 16:25:07 +0100

    Updated project

--- app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
+++ app/src/main/java/com/wohlmuth/onenote2021/ListActivity.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 import android.widget.BaseAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
-class ListActivity : AppCompatActivity(), View.OnClickListener {
+class ListActivity : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemClickListener {
 
     var noteAdapter: BaseAdapter? = null
 
@@ -20,34 +21,18 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
         // Find view by id
         val listView = findViewById<ListView>(R.id.lvNotes)
 
-        // Create list of strings
-        val notes = listOf(
-            "Pizza",
-            "Döner",
-            "Cars",
-            "Guns",
-            "Girls",
-            "Brokkoli",
-            "Pizza",
-            "Döner",
-            "Cars",
-            "Guns",
-            "Girls",
-            "Brokkoli",
-            "Pizza",
-            "Döner",
-            "Cars",
-            "Guns",
-            "Girls",
-            "Brokkoli",
-            "Eis"
-        )
+        // Create list of notes
+        val notes: MutableList<Note> = ArrayList()
+        notes.add(Note(1, System.currentTimeMillis(),"Title", "Message"))
+        notes.add(Note(2, System.currentTimeMillis(),"Title1", "Message1"))
+        notes.add(Note(3, System.currentTimeMillis(),"Title2", "Message2"))
 
         // Init adapter
-        noteAdapter = NoteAdapter(this, notes)
+        //noteAdapter = NoteAdapter(this, notes)
 
         // Set adapter on listView
-        listView.adapter = noteAdapter
+        //listView.adapter = noteAdapter
+        listView.onItemClickListener = this
     }
 
     override fun onResume() {
@@ -75,5 +60,10 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         return true
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val intent = Intent(this, NoteEditActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/wohlmuth/onenote2021/Note.kt
@@ -0,0 +1,3 @@
+package com.wohlmuth.onenote2021
+
+data class Note(val id: Long, val timestamp: Long, var title: String, var message: String)
