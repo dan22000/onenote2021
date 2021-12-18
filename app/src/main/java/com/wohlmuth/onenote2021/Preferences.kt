da7a720e8dda348adfe51357445eac2d57482db8
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2021-12-18 12:38:51 +0100

    Updated project

--- app/src/main/java/com/wohlmuth/onenote2021/Preferences.kt
+++ app/src/main/java/com/wohlmuth/onenote2021/Preferences.kt
@@ -6,31 +6,8 @@ class Preferences {
 
     companion object {
         const val PREFERENCES_USER_LOGGED_IN = "user_logged_in"
-        const val PREFERENCES_NOTE_TITLE = "note_title"
-        const val PREFERENCES_NOTE_MESSAGE = "note_message"
         const val PREFERENCES_NAME = "preferences_note"
     }
-
-    fun setNoteTitle(context: Context, noteTitle: String?) {
-        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-        preferences.edit().putString(PREFERENCES_NOTE_TITLE, noteTitle).apply()
-    }
-
-    fun getNoteTitle(context: Context): String? {
-        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-        return preferences.getString(PREFERENCES_NOTE_TITLE, null)
-    }
-
-    fun setNoteMessage(context: Context, noteMessage: String?) {
-        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-        preferences.edit().putString(PREFERENCES_NOTE_MESSAGE, noteMessage).apply()
-    }
-
-    fun getNoteMessage(context: Context): String? {
-        val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-        return preferences.getString(PREFERENCES_NOTE_MESSAGE, null)
-    }
-
     fun setUserLoggedIn(context: Context, loggedIn: Boolean) {
         val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         preferences.edit().putBoolean(PREFERENCES_USER_LOGGED_IN, loggedIn).apply()
